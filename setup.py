2e86b21d96cf839f3b77a091d97557b01788acb5
Benjamin Bastian <benjamin.bastian@gmail.com>
2017-08-16 14:24:05 -0700

    Add setuptools support

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name = 'selectable',
+    version = '0.0.1',
+    author = 'Benjamin Bastian',
+    description = 'A library for select-able objects',
+    license = 'MIT',
+    packages = ['selectable'],
+    url = 'https://github.com/sagelywizard/selectable'
+)
